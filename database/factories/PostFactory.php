236eabab9ed653daeffc58c00860db9a2b804a33
Aryan Gomes <aryan.gomes@gmail.com>
2022-07-02 11:03:13 -0300

    feat: Add Posts feature

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,13 +2,16 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
+    use WithFaker;
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker()->word(),
+            'content' => $this->faker()->paragraph(),
+            'user_id' => User::factory()->create(),
         ];
     }
 }
--- database/migrations/2022_07_01_205142_create_posts_table.php
+++ database/migrations/2022_07_01_205142_create_posts_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
